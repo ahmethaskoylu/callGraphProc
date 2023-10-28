cdaa55077f64d0bf00c601aa50496f17a39b4935
Ahmet Can Hasköylü <66915532+ahmethaskoylu@users.noreply.github.com>
2023-10-28 21:29:15 +0300

    Update clone_repo.py
    clone repo updated

--- clone_repo.py
+++ clone_repo.py
@@ -1,9 +1,6 @@
 import subprocess
 
-repo_url = "https://github.com/Chetan496/cpp-algortihms.git"
-
-
-def clone_github_repo(repo_url, commit_hash=None):
+def fetch_github_repo(repo_url, commit_hash=None):
     # Clone the repo
     repo_name = repo_url.split("/")[-1].replace(".git", "")
     subprocess.run(["git", "clone", repo_url])
@@ -14,5 +11,7 @@ def clone_github_repo(repo_url, commit_hash=None):
     return repo_name
 
 if __name__ == "__main__":
-
-    repo_name = clone_github_repo(repo_url)
+    repo_url = input("Enter the GitHub repository URL: ")
+    commit_hash = input("Enter the commit hash (optional): ")
+    repo_name = fetch_github_repo(repo_url, commit_hash)
+    print(f"Repository {repo_name} has been cloned.")
