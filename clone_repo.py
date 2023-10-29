d312ea667fb90ed1a67a02ee0a8ba624641f3118
ah021124_hw3 <can.haskoylu@ozu.edu.tr>
2023-10-29 10:20:44 +0300

    1-visualize 2-script 3-commit id these are updated

--- clone_repo.py
+++ clone_repo.py
@@ -1,17 +1,41 @@
 import subprocess
+import shutil
+import os
 
-def fetch_github_repo(repo_url, commit_hash=None):
-    # Clone the repo
+
+def fetch_github_repo(repo_url, commit_hashes=None):
     repo_name = repo_url.split("/")[-1].replace(".git", "")
     subprocess.run(["git", "clone", repo_url])
 
-    if commit_hash:
-        subprocess.run(["git", "-C", repo_name, "checkout", commit_hash])
+    if commit_hashes:
+        for commit_hash in commit_hashes:
+            # Checkout the specific commit
+            subprocess.run(["git", "-C", repo_name, "checkout", commit_hash])
+
+            # Create a directory for this specific commit
+            commit_dir = f"{repo_name}_{commit_hash}"
+            if os.path.exists(commit_dir):
+                shutil.rmtree(commit_dir)
+            os.makedirs(commit_dir)
+
+            # Copy the contents of the repo (excluding the .git folder) to the commit directory
+            for item in os.listdir(repo_name):
+                s = os.path.join(repo_name, item)
+                d = os.path.join(commit_dir, item)
+                if os.path.isdir(s):
+                    if ".git" not in s:
+                        shutil.copytree(s, d, False, None)
+                else:
+                    shutil.copy2(s, d)
+
+            print(f"Repository content at commit {commit_hash} has been copied to {commit_dir}")
 
     return repo_name
 
+
 if __name__ == "__main__":
     repo_url = input("Enter the GitHub repository URL: ")
-    commit_hash = input("Enter the commit hash (optional): ")
-    repo_name = fetch_github_repo(repo_url, commit_hash)
+    commit_hashes = input("Enter the commit hashes (optional, separated by spaces): ")
+    commit_hashes = commit_hashes.split() if commit_hashes else None
+    repo_name = fetch_github_repo(repo_url, commit_hashes)
     print(f"Repository {repo_name} has been cloned.")
--- visualize_graph.py
+++ visualize_graph.py
@@ -1,29 +1,28 @@
+import matplotlib
+matplotlib.use('MacOSX')
 import networkx as nx
 import matplotlib.pyplot as plt
-import matplotlib
+import sys
 
-matplotlib.use('MacOSX')
 
 def process_call_graph(callgraph_file):
-    G = nx.DiGraph()  # Create a directed graph
-
+    G = nx.DiGraph()
     with open(callgraph_file, 'r') as f:
         lines = f.readlines()
-
     stack = []
     for line in lines:
         level = len(line) - len(line.lstrip())
         func_name = line.strip().split()[0]
-
-        while len(stack) > level:  # Remove deeper or same level items
+        while len(stack) > level:
             stack.pop()
-
-        if stack:  # if stack is not empty, there is a parent
+        if stack:
             G.add_edge(stack[-1], func_name)
-
         stack.append(func_name)
-
     return G
+
+def calculate_metrics(G):
+    degree_centrality = nx.degree_centrality(G)
+    return degree_centrality
 
 def visualize_graph(G, metrics):
     pos = nx.shell_layout(G)
@@ -35,13 +34,48 @@ def visualize_graph(G, metrics):
     plt.figure(figsize=(12, 12))
     plt.show()
 
-def calculate_metrics(G):
-    degree_centrality = nx.degree_centrality(G)
-    return degree_centrality
+
+def visualize_graph_comparison(G_previous, G_current):
+    pos = nx.spring_layout(G_current)
+
+    # Nodes
+    added_nodes = [node for node in G_current.nodes() if node not in G_previous.nodes()]
+    removed_nodes = [node for node in G_previous.nodes() if node not in G_current.nodes()]
+    common_nodes = [node for node in G_current.nodes() if node in G_previous.nodes()]
+
+    nx.draw_networkx_nodes(G_current, pos, nodelist=added_nodes, node_color='green', node_size=500, alpha=0.6,
+                           label='Added')
+    nx.draw_networkx_nodes(G_current, pos, nodelist=removed_nodes, node_color='red', node_size=500, alpha=0.6,
+                           label='Removed')
+    nx.draw_networkx_nodes(G_current, pos, nodelist=common_nodes, node_color='blue', node_size=500, alpha=0.6,
+                           label='Unchanged')
+
+    nx.draw_networkx_labels(G_current, pos, font_size=8)
+
+    # Edges
+    added_edges = [edge for edge in G_current.edges() if edge not in G_previous.edges()]
+    removed_edges = [edge for edge in G_previous.edges() if edge not in G_current.edges()]
+    common_edges = [edge for edge in G_current.edges() if edge in G_previous.edges()]
+
+    nx.draw_networkx_edges(G_current, pos, edgelist=added_edges, edge_color='green', width=1, alpha=0.6)
+    nx.draw_networkx_edges(G_current, pos, edgelist=removed_edges, edge_color='red', width=1, alpha=0.6)
+    nx.draw_networkx_edges(G_current, pos, edgelist=common_edges, edge_color='blue', width=1, alpha=0.6)
+
+    plt.legend()
+    plt.show()
 
 if __name__ == "__main__":
-    callgraph_file = input("Enter the call graph file name: ")
-    G = process_call_graph(callgraph_file)
-    metrics = calculate_metrics(G)
-    visualize_graph(G, metrics)
-    print(metrics)
+    visualization_type = input("Enter the visualization type (1 for comparison, 2 for single): ")
+    if visualization_type == "1":
+        callgraph_file_previous = input("Enter the previous call graph file name: ")
+        callgraph_file_current = input("Enter the current call graph file name: ")
+        G_previous = process_call_graph(callgraph_file_previous)
+        G_current = process_call_graph(callgraph_file_current)
+        visualize_graph_comparison(G_previous, G_current)
+    elif visualization_type == "2":
+        callgraph_file = input("Enter the call graph file name: ")
+        G = process_call_graph(callgraph_file)
+        metrics = calculate_metrics(G)
+        visualize_graph(G, metrics)
+    else:
+        print("Invalid visualization type.")
