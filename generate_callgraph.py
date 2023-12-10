f65a0102cad76df17f9cdcc8fb04ec714cc117d9
tugberkcil <tugberk.cil@ozu.edu.tr>
2023-12-10 23:59:20 +0300

    Printing the number of times, on which line and the paths used by malloc, calloc and free functions in the functions_stats.json file

--- generate_callgraph.py
+++ generate_callgraph.py
@@ -34,23 +34,66 @@ def generate_call_graph(repo_name):
 
 # malloc, calloc, free fonksiyonlarını bulma ve sayma
 def count_memory_functions(file_name):
-    memory_functions_count = {
-        'malloc': 0,
-        'calloc': 0,
-        'free': 0
+    memory_functions = {
+        'malloc': [],
+        'calloc': [],
+        'free': []
     }
+
+    main_found = False
+    main_line = 0
+    line_number = 0
 
     with open(file_name, 'r') as file:
         lines = file.readlines()
         for line in lines:
-            if 'malloc' in line:
-                memory_functions_count['malloc'] += 1
-            if 'calloc' in line:
-                memory_functions_count['calloc'] += 1
-            if 'free' in line:
-                memory_functions_count['free'] += 1
+            line_number += 1
+            if 'main' in line.lower():
+                main_found = True
+                main_line = line_number
 
-    return memory_functions_count
+            if main_found:
+                if 'malloc' in line:
+                    distance = line_number - main_line
+                    paths = find_paths_to_main(main_line, line_number, lines)
+                    memory_functions['malloc'].append({'line_number': line_number, 'distance_to_main': distance})
+                if 'calloc' in line:
+                    distance = line_number - main_line
+                    paths = find_paths_to_main(main_line, line_number, lines)
+                    memory_functions['calloc'].append({'line_number': line_number, 'distance_to_main': distance})
+                if 'free' in line:
+                    distance = line_number - main_line
+                    paths = find_paths_to_main(main_line, line_number, lines)
+                    memory_functions['free'].append({'line_number': line_number, 'distance_to_main': distance})
+
+    return memory_functions
+
+def find_paths_to_main(main_line, target_line, lines):
+    paths = []
+
+    def backtrack(current_line, current_path):
+        if current_line == main_line:
+            paths.append(list(current_path))
+            return
+        elif current_line < main_line:
+            return
+
+        # Geriye git ve geçişler üzerinden yolları keşfet
+        for i in range(current_line - 1, 0, -1):
+            if lines[i - 1].strip().endswith(';') or lines[i - 1].strip().endswith('}'):
+                current_path.append(i)
+                backtrack(i, current_path)
+                current_path.pop()
+
+    # Başlangıç olarak, hedef satıra kadar olan yolları bul
+    backtrack(target_line, [])
+
+    # Yolları tersine çevir, çünkü yollar geriye doğru bulunuyor
+    reversed_paths = [list(reversed(path)) for path in paths]
+
+    return reversed_paths
+
+
 
 
 # main ve program çıkışını temsil eden fonksiyonları bulma ve sayma
@@ -142,32 +185,29 @@ if __name__ == "__main__":
             current_callgraph = data['call_graphs']['current']
 
         functions_statistics = find_functions_statistics(current_callgraph)
-        memory_functions_count = count_memory_functions(current_callgraph)
+        memory_functions_distance = count_memory_functions(current_callgraph)
         entry_exit_functions_count = count_entry_exit_functions(current_callgraph)
 
-        # JSON dosyasına yazma
+        memory_functions_data = {}
+        for func_name, line_numbers in memory_functions_distance.items():
+            distance = ",".join(str(x['line_number']) for x in line_numbers)
+            paths = ",".join(str(x['distance_to_main']) for x in line_numbers)
+            count = len(line_numbers)
+            memory_functions_data[func_name] = {
+                'count': count,
+                'line_number': distance,
+                'paths': paths #her line'ın maine uzaklığı
+            }
+
         functions_count = {
-            'memory_functions': memory_functions_count,
+            'memory_functions': memory_functions_data,
             'entry_exit_functions': entry_exit_functions_count,
             'func_stat': functions_statistics
         }
 
-        # JSON dosyasına yazma
         with open('functions_stats.json', 'w') as json_file:
             json.dump(functions_count, json_file, indent=4)
 
-        with open('config.json', 'r') as config_file:
-            config_data = json.load(config_file)
-
-        # functions_stats.json dosyasını oku
-        with open('functions_stats.json', 'r') as functions_count_file:
-            functions_stats_data = json.load(functions_count_file)
-
-        # Verileri birleştir
-        merged_data = {**config_data, **functions_stats_data}
-
-        # config.json dosyasına yaz
-        with open('config.json', 'w') as config_file:
-            json.dump(merged_data, config_file, indent=4)
 
 
+            #cpp-algortihms
